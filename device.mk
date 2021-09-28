62ef71f60394476344368988fdda6dbd98dd3b9e
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-09-28 19:50:02 +0530

    surya: Set default Settings.Global.DEVICE_NAME for karna & surya
     * Reference:
       https://github.com/AdarshGrewal/android_device_xiaomi_sweet/commit/a4524e8d6648ec8e7f98e7d779d11eb98a0f5103
    
    Change-Id: I42c22c32f323c62e51204507c3eb5cb1ef037ad3

--- device.mk
+++ device.mk
@@ -352,6 +352,8 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
     KarnaPowerProfile \
+    KarnaSettingsProviderOverlay \
+    SuryaSettingsProviderOverlay \
     NotchBarKiller
 
 # Partitions
