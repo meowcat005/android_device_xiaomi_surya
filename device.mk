a1e71036c77317762247b62739bc41dd5f85278d
luk1337 <priv.luk@gmail.com>
2022-01-05 11:45:23 -0300

    surya: Import QTI vibrator HAL
    Change-Id: I6ed3252c6da746c8fb24f44b3b3ec2b1f32a6cb2

--- device.mk
+++ device.mk
@@ -488,6 +488,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.xiaomi_sm6150
+
 # VNDK
 PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
