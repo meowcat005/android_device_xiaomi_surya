bdeff83e4b7fa3a781e0b06fe30e4e85a40e5af4
Trickbot199 <mezackisilva@gmail.com>
2022-01-07 14:21:51 -0300

    Revert "surya: redfin december fp"
    This reverts commit 688cdaacd6050bec126e23341523250b70cff603.

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -55,6 +55,16 @@ void property_override(char const prop[], char const value[], bool add = true)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void set_ro_build_prop(const std::string &prop, const std::string &value) {
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro." + source + "build." + prop;
+        if (source == "")
+            property_override(prop_name.c_str(), value.c_str());
+        else
+            property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
+
 void set_ro_product_prop(const std::string &prop, const std::string &value) {
     for (const auto &source : ro_props_default_source_order) {
         auto prop_name = "ro.product." + source + prop;
@@ -88,6 +98,7 @@ void vendor_load_properties() {
 
     std::string model;
     std::string device;
+    std::string fingerprint;
     std::string description;
     std::string name;
     std::string mod_device;
@@ -97,15 +108,22 @@ void vendor_load_properties() {
         model = "M2007J20CG";
         mod_device = "surya_global";
 	name = "surya_global";
+	fingerprint = "POCO/surya_eea/surya:11/RKQ1.200826.002/V12.5.4.0.RJGMIXM:user/release-keys";
 	description = "surya_eea-user 11 RKQ1.200826.002 V12.5.4.0.RJGMIXM release-keys";
     } else if (hwname == "karna") {
 	device = "karna";
         model = "M2007J20CI";
         mod_device = "surya_in_global";
 	name = "karna_in";
+	fingerprint = "POCO/surya_in/karna:11/RKQ1.200826.002/V12.5.4.0.RJGMIXM:user/release-keys";
 	description = "surya_in-user 11 RKQ1.200826.002 V12.5.4.0.RJGMIXM release-keys";
    }
 
+   // SafetyNet workaround
+   fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
+   description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
+
+   set_ro_build_prop("fingerprint", fingerprint);
    set_ro_product_prop("brand", "POCO");
    set_ro_product_prop("device", device);
    set_ro_product_prop("product", device);
--- voltage_surya.mk
+++ voltage_surya.mk
@@ -35,10 +35,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.211205.008 7888514 release-keys"
-
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys
-
 # Official-ify
 VOLTAGE_BUILD_TYPE := OFFICIAL
